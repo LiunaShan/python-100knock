ac698c5187d341a3673ed7032363c843373db263
LiunaShan <LiunaShan@users.noreply.github.com>
2026-02-23 18:56:32 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,83 @@
+#1
+print("#1")
+print( "Hello, World!")
+#2
+print("#2")
+print(1)
+print(2)
+print(3)
+#3
+print("#3")
+print(123,"Hello")
+#4
+print("#4")
+print(1)
+#print(2)
+print(3)
+#5
+print("#5")
+"""
+print(1)
+print(2)
+print(3)
+"""
+
+
+#6
+print("#6")
+x = 10
+print(x)
+#7
+print("#7")
+x = 1 + 2
+print(x)
+
+#8
+print("#8")
+x = "1" + "2"
+print(x)
+
+#9
+print("#9")
+print(str(10))
+print(int("10"))
+
+#10
+print("#10")
+print("1\n2\n3")
+
+#11
+print("#11")
+print("C:\\test")
+
+#12
+print("#12")
+print(r"C:\test")
+
+#13
+print("#13")
+name = "Python"
+print("Hello," + name + "!")
+
+#14
+print("#14")
+x = "Python"
+print(len(x))
+
+#15
+print("#15")
+print(x[0],x[1])
+print(x[0:2])
+
+#16
+print("#16")
+x = None
+print(x) 
+
+#17
+print("#17")
+a = 10
+b = 3.14
+c = "Hello, Python!"
+d = True
+print(type(a),type(b),type(c),type(d))
