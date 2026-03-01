5777ca5f0511fca5748734320e1364e7d206a287
LiunaShan <LiunaShan@users.noreply.github.com>
2026-03-02 00:07:26 +0900

    until #58

--- main3.py
+++ main3.py
@@ -42,4 +42,43 @@ print("#55")
 result =[ [ f"{i} - {j}" for j in range(10)] for i in range(10)]
 print(result)
 
+#56
+print("#56")
+ascii_dict = {'0x30': '0', '0x40': '@', '0x50': 'P'} 
+reversed_dict = {value: key for key, value in ascii_dict.items()}
+print(reversed_dict);
+
+#57
+print("#57")
+target_text = "hello"
+count_dic = {}
+for i in target_text:
+    if ( i in count_dic):
+        count_dic[i] += 1
+    else:
+        count_dic[i] = 1
+print(count_dic) 
+
+#58
+print("#58")
+target_list =  [1, 2, 3, None, 5, None, 7]
+#bad example
+print("bad example")
+a = ""
+b = len(target_list)
+c = 0
+for i in target_list:
+    c += 1
+    if i is not None and c is not b:
+        a = a + str(i) + "&"
+    elif i is not None and c == b:
+        a = a + str(i)
+print(a)
+#good example
+print("good example")
+filtered_list = [str(i) for i in target_list if i is not None]
+result = '&'.join(filtered_list)
+print(result)
+
+
 
